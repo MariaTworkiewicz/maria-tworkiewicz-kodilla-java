e95894a429eb379c6c2fadacbed6841e5380b951
Maryna123 <maria.tworkiewicz@gmail.com>
2018-06-06 22:18:48 -0500

    calculator

---
+++ calculator/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,28 @@
+package com.example.calculator;
+
+public class Calculator {
+
+    private double x;
+    private double y;
+
+    public Calculator(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public double add(){
+        return x + y;
+    }
+
+    public double subtract(){
+        return x - y;
+    }
+}
---
+++ calculator/src/main/java/com/example/calculator/CalculatorApplication.java
@@ -0,0 +1,23 @@
+package com.example.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+
+        Calculator liczydlo = new Calculator(30, 6);
+
+        System.out.println(liczydlo.add());
+        System.out.println(liczydlo.subtract());
+    }
+
+
+
+
+
+}
