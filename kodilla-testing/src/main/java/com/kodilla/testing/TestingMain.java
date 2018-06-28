f79cbbc9b5f5cbc491d495da414e9194825fa37d
Maryna123 <maria.tworkiewicz@gmail.com>
2018-06-28 14:30:51 -0500

    Modul6.2 poprawka

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,8 +5,8 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
+        System.out.println("Testing SimpleUser");
         SimpleUser simpleUser = new SimpleUser("theForumUser");
-
         String result = simpleUser.getUsername();
 
         if(result.equals("theForumUser")){
@@ -16,9 +16,15 @@ public class TestingMain {
         }
 
         //test for Calculator class
+        System.out.println("Testing Calculator");
         Calculator calculator = new Calculator(5, 3);
-        System.out.println(calculator.getA() + " + " + calculator.getB() + " = " + calculator.add());
-        System.out.println(calculator.getA() + " - " + calculator.getB() + " = " + calculator.subtract());
+        int addResult = calculator.add();
+        int subtractResult = calculator.subtract();
 
+        if(addResult == 8 && subtractResult == 2){
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
     }
 }
