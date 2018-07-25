bb3484758828d235f0bd0b2558bcb9d30f890991
Maryna123 <maria.tworkiewicz@gmail.com>
2018-07-25 11:43:42 -0500

    modul 8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -18,14 +18,31 @@ public class FlightFinder {
         airportAvailability.put("Chicago", false);
         airportAvailability.put("New York", true);
         airportAvailability.put("Chicago", true);
+        airportAvailability.put("Katowice", true);
         Map<String, Boolean> theAirports = new HashMap<>();
         theAirports.putAll(airportAvailability);
         return theAirports;
     }
 
     public void findFlight(Flight flight) throws RouteNotFoundException {
-        if ((this.getAirportAvailability().get(flight.getArrivalAirport())) != null)
-            this.getAirportAvailability().entrySet().stream()
+        getAirportAvailability().entrySet().stream()
+                .filter(p -> p.getKey().equals(flight.getDepartureAirport()))
+                .findAny()
+                .orElseThrow(() -> new RouteNotFoundException("Unable to find Departure Airport: " + flight.getDepartureAirport()));
+
+        boolean canLanding = getAirportAvailability().entrySet().stream()
+                .filter(p -> p.getKey().equals(flight.getArrivalAirport()))
+                .findAny()
+                .map(v -> v.getValue())
+                .orElseThrow(() -> new RouteNotFoundException("Unable to find Arrival Airport: " + flight.getArrivalAirport()));
+        if(canLanding) {
+            System.out.println("You can fly from " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport());
+        } else {
+            System.out.print("I'm sorry, you cannot use: " + flight.getArrivalAirport() + " as your destination");
+        }
+
+      /*  if ((getAirportAvailability().get(flight.getArrivalAirport())) != null)
+            getAirportAvailability().entrySet().stream()
                     .forEach(
                             pair -> {
                                 if (pair.getKey().equals(flight.getArrivalAirport())) {
@@ -39,7 +56,7 @@ public class FlightFinder {
                     );
         else {
             throw new RouteNotFoundException("Destination airport not found: " + flight.getArrivalAirport());
-        }
+        }*/
 
    /* public void findFlight(Flight flight) throws RouteNotFoundException {
         airportAvailability.put("San Francisco", true);
