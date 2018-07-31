71cfdc1b095a2c1655db7b35d572d721c9beeea5
Maryna123 <maria.tworkiewicz@gmail.com>
2018-07-31 13:30:41 -0500

    Modul 10.3

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -0,0 +1,34 @@
+package com.kodilla.spring.portfolio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class BoardConfig {
+    @Autowired
+    Board board;
+
+    @Bean(name = "toDoList")
+    public TaskList getToDoTaskList(){
+        return new TaskList();
+    }
+
+    @Bean(name = "inProgressList")
+    public TaskList getinProgressTaskList(){
+        return new TaskList();
+    }
+
+    @Bean(name = "DoneList")
+    public TaskList getDoneTaskList(){
+        return new TaskList();
+    }
+
+
+    @Bean
+    public Board getBoard() {
+        return new Board(getToDoTaskList(), getinProgressTaskList(), getDoneTaskList());
+    }
+}
